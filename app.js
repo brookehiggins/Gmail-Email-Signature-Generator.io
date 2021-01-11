4f908ae784739d25b20aa2669e4df8adf6bd70aa
brookehiggins <brooke@cantforget.me>
2021-01-11 11:30:16 -0700

    For the public

---
+++ app.js
@@ -0,0 +1,64 @@
+
+
+updateNameValue = (e) => {
+    document.getElementById('name').innerHTML = e.target.value
+};
+
+const nameInput = document.getElementById('nameInput')
+nameInput.addEventListener('keyup', updateNameValue)
+
+
+updateTitleValue = (e) => {
+    document.getElementById('title').innerHTML = e.target.value
+};
+
+const titleInput = document.getElementById('titleInput')
+titleInput.addEventListener('keyup', updateTitleValue)
+
+updateCompanyValue = (e) => {
+    document.getElementById('company').innerHTML = e.target.value
+};
+
+const companyInput = document.getElementById('companyInput')
+companyInput.addEventListener('keyup', updateCompanyValue)
+
+
+updatePhoneNumberValue = (e) => {
+    document.getElementById('phoneNumber').innerHTML = e.target.value
+};
+
+const phoneNumberInput = document.getElementById('phoneNumberInput')
+phoneNumberInput.addEventListener('keyup', updatePhoneNumberValue)
+
+updateEmailValue = (e) => {
+    document.getElementById('email').innerHTML = e.target.value
+};
+
+const emailInput = document.getElementById('emailInput')
+emailInput.addEventListener('keyup', updateEmailValue)
+
+// const toBase64 = file => new Promise((resolve, reject) => {
+//     const reader = new FileReader();
+//     reader.readAsDataURL(file);
+//     reader.onload = () => resolve(reader.result);
+//     reader.onerror = error => reject(error);
+// });
+
+updateImageValue = (e) => {
+    document.getElementById('yourImage').src = e.target.value
+};
+
+const imageInput = document.getElementById('imageInput')
+imageInput.addEventListener('keyup', updateImageValue)
+
+
+updateButton = (e) => {
+    const value = document.getElementById('colorPicker').src = e.target.value;
+    const btn = document.getElementById('btn');
+    const line = document.getElementById('line');
+    btn.style['background-color'] = value;
+    line.style['color'] = value;
+};
+
+const picker = document.getElementById('colorPicker')
+picker.addEventListener('change', updateButton)
